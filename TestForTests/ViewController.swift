dc2da8fe966a494126085d2ec94fe5cd3549a4ee
Arthur Mikhaylov <amikhaylov@swisstech.io>
2019-03-09 23:53:23 +0300

    Initial Commit

---
+++ TestForTests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestForTests
+//
+//  Created by Arthur Mikhaylov on 09/03/2019.
+//  Copyright © 2019 Company name. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TestForTestsTests/TestForTestsTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestForTestsTests.swift
+//  TestForTestsTests
+//
+//  Created by Arthur Mikhaylov on 09/03/2019.
+//  Copyright © 2019 Company name. All rights reserved.
+//
+
+import XCTest
+@testable import TestForTests
+
+class TestForTestsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
